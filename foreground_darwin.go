e92ac513f036d674312970ebc985f2a61be909f2
LightningDev1 <59872288+LightningDev1@users.noreply.github.com>
2024-02-12 17:07:36 +0100

    feat: Add macOS implementation

---
+++ foreground_darwin.go
@@ -0,0 +1,23 @@
+// go:build darwin
+
+package foreground
+
+import (
+	"errors"
+
+	"github.com/progrium/macdriver/macos/appkit"
+)
+
+// GetForegroundPID returns the PID of the foreground window.
+func GetForegroundPID() (uint32, error) {
+	return 0, errors.New("GetForegroundPID() not implemented on darwin")
+
+}
+
+// GetForegroundTitle returns the title of the foreground window.
+func GetForegroundTitle() (string, error) {
+	workspace := appkit.Workspace_SharedWorkspace()
+	frontApp := workspace.FrontmostApplication()
+
+	return frontApp.LocalizedName(), nil
+}
