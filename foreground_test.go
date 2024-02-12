c8504ceb3ed183996f1b91e2412e44b7f46effb9
LightningDev1 <59872288+LightningDev1@users.noreply.github.com>
2024-02-12 17:16:53 +0100

    fix(test): Change Fatal to Error

--- foreground_test.go
+++ foreground_test.go
@@ -7,14 +7,14 @@ import (
 func TestForeground(t *testing.T) {
 	pid, err := GetForegroundPID()
 	if err != nil {
-		t.Fatal("GetForegroundPID():", err)
+		t.Error("GetForegroundPID():", err)
 	}
 
 	t.Log("Foreground PID:", pid)
 
 	title, err := GetForegroundTitle()
 	if err != nil {
-		t.Fatal("GetForegroundTitle():", err)
+		t.Error("GetForegroundTitle():", err)
 	}
 
 	t.Log("Foreground title:", title)
